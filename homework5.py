bab1942b80ba52c0a188efd3ded271ce3cd38da8
Александр <71317481+Aleksandr727@users.noreply.github.com>
2024-05-09 13:46:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'lemon', 'melon', 'tomato', 'banana', 'peach', 'mango']
+print(my_list)
+print(my_list[:1])
+print(my_list[-1:])
+print(my_list[3:5])
+my_list[2]='fish'
+print(my_list)
+
+my_dict = {'seven': 7, 'fourteen': 14}
+print(my_dict)
+print(my_dict['seven'])
+my_dict['seven'] = 8
+print(my_dict)
