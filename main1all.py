86ddc80970130f71593c0755e52b5a85adfb21ca
Александр <71317481+Aleksandr727@users.noreply.github.com>
2024-06-04 13:22:51 +0300

    Add files via upload

---
+++ main1all.py
@@ -0,0 +1,24 @@
+# Задача «Длина слова».
+a = 'Сила слова в его реализации'
+print(len(a))
+# Задача «Суммы и разности».
+first = 123
+second = 234
+summa = first+second
+diff = first-second
+print(summa)
+print(diff)
+# Задача «Среднее арифметическое».
+third = 888
+mean =(first+second+third)/3
+print('Среднее арифметическое',mean)
+# Задача «Простые строчки».
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string,', ',first_string)
+# Задача «Сложная формула».
+a = 7
+b = 4
+c = 8
+f = (((a*b) + (a*c))**3)/2
+print(f)
