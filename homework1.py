1e0150bccb565a51cabfd51fa05af81cb8df983a
Александр <71317481+Aleksandr727@users.noreply.github.com>
2024-06-06 07:56:46 +0300

    homework1.py

--- homework1.py
+++ homework1.py
@@ -4,3 +4,5 @@ print(example[-1])
 print(example[2::3])
 print(example[::-1])
 print(example[0::2])
+#думаю будет тоже правильно, чтоб был каждый второй символ, так как "A" не является вторым символом.
+print(example[1::2])
