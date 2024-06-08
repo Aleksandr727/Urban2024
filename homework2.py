fb9b716bbd3081b55f753cb8448692bf2e09550a
Александр <71317481+Aleksandr727@users.noreply.github.com>
2024-06-08 22:53:47 +0300

    homework2.py

--- homework2.py
+++ homework2.py
@@ -2,4 +2,4 @@ Number_of_homework = 12
 Number_of_hours = 1.5
 Course_name = 'Курс: Python'
 Time_for_one_homework = Number_of_hours / Number_of_homework
-print(Course_name,', всего задач:',N
+print(Course_name,', всего задач:',Number_of_homework,', затрачено часов:',Number_of_hours,', среднее время выполнения',Time_for_one_homework, 'часа')
