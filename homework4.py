ea7e550d5bc2dec14e1a5cebae28934dab8c5029
Александр <71317481+Aleksandr727@users.noreply.github.com>
2024-06-09 12:57:37 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,13 +1,8 @@
-immutable_var = 7, 8, 9, False, 'Samuray'
-immutable_var_2 = (7, 8, 9, False, 'Samuray')
-immutable_var_3 = [7, 8, 9, False, 'Samuray']
-print(immutable_var)
-print(immutable_var_2)
-print(immutable_var_3)
-print(type(immutable_var))         # Особенность кортежа в том, что он не изменяемый!!! <class 'tuple'>
-print(immutable_var[4])
-# immutable_var[4]=700               # Выдает ошибку при попытке поменять!!!
-
-mutable_list = [9, 7, 4, True, 'Defolt']
-mutable_list[3]= 444
-print(mutable_list)
+my_string = input('Как ваше самочувствие? ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print('Чтоб всегда', my_string)
+print('Чтоб всегда'.replace(' ',''))
+print(my_string[0:1])
+print(my_string[-1])
